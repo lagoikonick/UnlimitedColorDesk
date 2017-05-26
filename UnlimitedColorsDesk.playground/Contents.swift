6779169a8e175f46fbced8d690d7dfb83636671c
Lagoiko Nick <lagoiko.nick@gmail.com>
2017-05-26 04:54:41 +0300

    updating color array
    Заполнил массив нормальными цветами (6 штук)

--- UnlimitedColorsDesk.playground/Contents.swift
+++ UnlimitedColorsDesk.playground/Contents.swift
@@ -1,7 +1,6 @@
-let colorsCount = 2
-let coordinates = (x: 1, y: 6)
-let colors = ["ЖОЛЬТАМЭ", "ЗЫЛИНОВЫ", "ХТОТУТЦВИТНИВЕДАЕ", "СНОВАЖОЛЬТАМАТЮЛЬПАНЭ", "ЗАКАНЧИУАЕМ"]
-
+let colorsCount = 5
+let coordinates = (x: 1965, y: 451)
+let colors = ["№1 - #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)", "№2 - #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)", "№3 - #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)", "№4 - #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)", "№5 - #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)", "№6 - #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)"]
 
 
 var color = (( coordinates.x + coordinates.y ) % colorsCount) - 1
